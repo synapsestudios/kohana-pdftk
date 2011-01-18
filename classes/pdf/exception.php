9088e1e502a3924d6d440a0745245ab6e84b8521
areida <andrew@syn0.com>
2011-01-18 16:01:06 -0700

    Adding PDF exception.

---
+++ classes/pdf/exception.php
@@ -0,0 +1,3 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class PDF_Exception extends Synapse_PDF_Exception {}
---
+++ classes/synapse/pdf/exception.php
@@ -0,0 +1,3 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class Synapse_PDF_Exception extends RuntimeException {}
